85aaf17b0dcb557c5527fe9284c0c3032e3de99d
William Diaz <williamandresd@gmail.com>
2025-09-08 11:12:30 -0500

    añade leyendas tipo gherkin para un mejor entendimiento en el reporte

--- src/utils/constants/api-messages.constants.ts
+++ src/utils/constants/api-messages.constants.ts
@@ -24,3 +24,7 @@ export enum ApiSuccessMessages {
   USER_REGISTERED = '✓ Usuario registrado exitosamente',
   ERROR_EXPECTED = '✓ Error esperado validado correctamente'
 }
+
+export const API_VALIDATION_PATTERNS = {
+  AUTH_TOKEN_REGEX: /^Auth_token:\s+[A-Za-z0-9+/=]+$/
+} as const;
--- tests/reto_tecnico.api.spec.ts
+++ tests/reto_tecnico.api.spec.ts
@@ -1,7 +1,30 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, APIResponse } from '@playwright/test';
 import { DEMOBLAZE_URLS } from '../src/utils/constants/api-urls.constants';
-import { DemoblazeErrorMessages, HttpStatus, HEADERS, ApiSuccessMessages } from '../src/utils/constants/api-messages.constants';
+import { DemoblazeErrorMessages, HttpStatus, HEADERS, ApiSuccessMessages, API_VALIDATION_PATTERNS } from '../src/utils/constants/api-messages.constants';
 import { ApiCredential, API_TEST_USERS, generateUniqueUser } from '../src/utils/constants/api-users.constants';
+
+/**
+  Feature: Validación de API REST para Demoblaze
+    Como QA Automatizador de pruebas
+    Quiero validar las funcionalidades de la API de Demoblaze
+    Para asegurar que los endpoints de registro y autenticación funcionan correctamente
+
+  Scenario: Registro exitoso de nuevo usuario
+    When envío una petición POST al endpoint de registro con credenciales nuevas y validas
+    Then debería recibir un status 200 y un cuerpo de respuesta vacío
+
+  Scenario: Validación de usuario existente
+    When intento registrar un usuario ya registrado
+    Then debería recibir un mensaje de error notificando que el usuario ya existe
+
+  Scenario: Autenticación con credenciales válidas
+    When envío una petición POST al endpoint de login con credenciales válidas
+    Then debería recibir un token de autenticación válido
+
+  Scenario: Autenticación con credenciales inválidas
+    When envío una petición POST al endpoint de login con credenciales invalidas
+    Then debería recibir un mensaje de error
+ */
 
 let credentials: ApiCredential;
 
@@ -11,87 +34,102 @@ test.describe.serial('Pruebas API para Demoblaze', () => {
   })
 
   test('Validar el registro exitoso de un nuevo usuario', async ({ request }) => {
-    credentials = generateUniqueUser();
+    let response: APIResponse
 
-    const response = await request.post(DEMOBLAZE_URLS.SIGNUP, {
-      headers: HEADERS.CONTENT_TYPE_JSON,
-      data: {
-        username: credentials.user,
-        password: credentials.password,
-      },
+    await test.step('When envío una petición POST al endpoint de registro con credenciales nuevas y válidas', async () => {
+      credentials = generateUniqueUser();
+      response = await request.post(DEMOBLAZE_URLS.SIGNUP, {
+        headers: HEADERS.CONTENT_TYPE_JSON,
+        data: {
+          username: credentials.user,
+          password: credentials.password,
+        },
+      });
     });
 
-    //TODO añadir a conclusiones que no tiene buenas prácticas los status
-    expect(response.status()).toBe(HttpStatus.OK);
-    const body = await response.json();
-    expect(Object.keys(body).length).toBe(0);// La API de demoblaze devuelve un objeto vacío {} para un registro exitoso.
-    expect(body).not.toHaveProperty('errorMessage');
-    console.log(ApiSuccessMessages.USER_REGISTERED);//TODO cambiar el console.log por log de playwright
+    await test.step('Then debería recibir un status 200 y un cuerpo de respuesta vacío', async () => {
+      //TODO añadir a conclusiones que no tiene buenas prácticas los status
+      expect(response.status()).toBe(HttpStatus.OK);
+      const body = await response.json();
+      expect(Object.keys(body).length).toBe(0);// La API de demoblaze devuelve un objeto vacío {} para un registro exitoso.
+      expect(body).not.toHaveProperty('errorMessage');
+      console.log(ApiSuccessMessages.USER_REGISTERED);//TODO cambiar el console.log por log de playwright
+    });
+
   });
 
   test('Validar que no se permita crear un usuario existente', async ({ request }) => {
-    const existingUser = API_TEST_USERS.EXISTING_USER;
+    let response: APIResponse;
 
-    const response = await request.post(DEMOBLAZE_URLS.SIGNUP, {
-      headers: HEADERS.CONTENT_TYPE_JSON,
-      data: {
-        username: existingUser.user,
-        password: existingUser.password,
-      },
+    await test.step('When intento registrar un usuario ya registrado', async () => {
+      response = await request.post(DEMOBLAZE_URLS.SIGNUP, {
+        headers: HEADERS.CONTENT_TYPE_JSON,
+        data: {
+          username: API_TEST_USERS.EXISTING_USER.user,
+          password: API_TEST_USERS.EXISTING_USER.password,
+        },
+      });
     });
 
-    expect(response.status()).toBe(HttpStatus.OK);
+    await test.step('Then debería recibir un mensaje de error notificando que el usuario ya existe', async () => {
+      expect(response.status()).toBe(HttpStatus.OK);
 
-    const body = await response.json();
-    console.log(ApiSuccessMessages.ERROR_EXPECTED);
+      const body = await response.json();
+      console.log(ApiSuccessMessages.ERROR_EXPECTED);
 
-    expect(body).toHaveProperty('errorMessage');
-    expect(body.errorMessage).toBe(DemoblazeErrorMessages.USER_ALREADY_EXISTS);
+      expect(body).toHaveProperty('errorMessage');
+      expect(body.errorMessage).toBe(DemoblazeErrorMessages.USER_ALREADY_EXISTS);
+    });
+
   });
 
   test('Validar la autenticación con credenciales válidas', async ({ request }) => {
 
+    let responseLogin: APIResponse;
+    await test.step('When envío una petición POST al endpoint de login con credenciales válidas', async () => {
 
-    const responseLogin = await request.post(DEMOBLAZE_URLS.LOGIN, {
-      headers: HEADERS.CONTENT_TYPE_JSON,
-      data: {
-        username: credentials?.user ?? API_TEST_USERS.EXISTING_USER.user,
-        password: credentials?.password ?? API_TEST_USERS.EXISTING_USER.password
-      },
+      responseLogin = await request.post(DEMOBLAZE_URLS.LOGIN, {
+        headers: HEADERS.CONTENT_TYPE_JSON,
+        data: {
+          username: credentials?.user ?? API_TEST_USERS.EXISTING_USER.user,
+          password: credentials?.password ?? API_TEST_USERS.EXISTING_USER.password
+        },
+      });
     });
 
+    await test.step('Then debería recibir un token de autenticación válido', async () => {
+      expect(responseLogin.status()).toBe(HttpStatus.OK);
 
-    expect(responseLogin.status()).toBe(HttpStatus.OK);
+      const bodyLogin = await responseLogin.json();
 
-  
-    const bodyLogin = await responseLogin.json();
+      expect(bodyLogin).not.toBeNull();
+      expect(Object.keys(bodyLogin).length).toBeGreaterThan(0);
+      expect(bodyLogin).not.toHaveProperty('errorMessage');
+      expect(bodyLogin).toMatch(API_VALIDATION_PATTERNS.AUTH_TOKEN_REGEX);
+    });
 
-
-    expect(bodyLogin).not.toBeNull();
-    expect(Object.keys(bodyLogin).length).toBeGreaterThan(0);
-    expect(bodyLogin).not.toHaveProperty('errorMessage');
-    expect(bodyLogin).toMatch(/^Auth_token:\s+[A-Za-z0-9+/=]+$/) //TODO PASAR A CONSTANTES
   });
 
   test('Validar que no se permita la autenticación con credenciales inválidas', async ({ request }) => {
-    const invalidUser = API_TEST_USERS.INVALID_USER;
-
-    const response = await request.post(DEMOBLAZE_URLS.LOGIN, {
-      headers: HEADERS.CONTENT_TYPE_JSON,
-      data: {
-        username: invalidUser.user,
-        password: invalidUser.password,
-      },
+    let response: APIResponse;
+    await test.step('When envío una petición POST al endpoint de login con credenciales invalidas', async () => {
+      response = await request.post(DEMOBLAZE_URLS.LOGIN, {
+        headers: HEADERS.CONTENT_TYPE_JSON,
+        data: {
+          username: API_TEST_USERS.INVALID_USER.user,
+          password: API_TEST_USERS.INVALID_USER.password,
+        },
+      });
     });
 
-    expect(response.status()).toBe(HttpStatus.OK);
+    await test.step('Then debería recibir un mensaje de error', async () => {
+      expect(response.status()).toBe(HttpStatus.OK);
+      const body = await response.json();
+      expect(body).toHaveProperty('errorMessage');
+      expect(body.errorMessage).not.toBeNull();
+      console.log(ApiSuccessMessages.ERROR_EXPECTED);//TODO cambiar a log de playwright
+    });
 
-    const body = await response.json();
-
-
-    expect(body).toHaveProperty('errorMessage');
-    expect(body.errorMessage).not.toBeNull();
-    console.log(ApiSuccessMessages.ERROR_EXPECTED);//TODO cambiar a log de playwright
   });
 
 
